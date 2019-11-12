eb4e1cc027e63803105eab80e2f81dde8da5e0f6
serhii <kukhar.s707@gmail.com>
2019-11-12 22:06:42 +0200

    Updates

---
+++ precache-manifest.672e98c3e0c893483b8b2ec8cdac4dc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9383530a48bbc60d4270748efadba523",
+    "url": "/widget/index.html"
+  },
+  {
+    "revision": "43eec5c1a578004fd2a8",
+    "url": "/widget/static/css/2.27cde75d.chunk.css"
+  },
+  {
+    "revision": "e0e80e2087c87430a1b4",
+    "url": "/widget/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "43eec5c1a578004fd2a8",
+    "url": "/widget/static/js/2.17b77e76.chunk.js"
+  },
+  {
+    "revision": "e0e80e2087c87430a1b4",
+    "url": "/widget/static/js/main.33646257.chunk.js"
+  },
+  {
+    "revision": "d576f3518d6a53a94c49",
+    "url": "/widget/static/js/runtime-main.31c36d51.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/widget/static/media/persik.4e1ec840.png"
+  }
+]);
